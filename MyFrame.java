d42c9bdab480f1b363d31254a57f104b39436a3c
ItayosP <itaypollack3@gmail.com>
2023-12-03 20:48:59 +0200

    i swear this project drives me crazyyyyyy

--- MyFrame.java
+++ MyFrame.java
@@ -1,7 +1,13 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener{
+
+    JButton myButton1 = new JButton();
+    JButton myButton2 = new JButton();
+
     MyFrame() {
 
         // Design part
@@ -12,7 +18,30 @@ public class MyFrame extends JFrame {
         // Settings part
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null); // Setting layout manager to null for manual component positioning
+        this.setLayout(null);
         this.setVisible(true);
+
+        myButton1.addActionListener(this);
+        myButton2.addActionListener(this);
+
+        //button1
+        myButton1.setBounds(463, 210, 75, 50);
+        myButton1.setVisible(true); 
+        
+        //button2
+        myButton2.setBounds(463, 500, 75, 50);
+        myButton2.setVisible(true);
+
+        this.add(myButton1);
+        this.add(myButton2);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==myButton1) {
+            System.out.println("Button 1 clicked");
+        } else if(e.getSource()==myButton2) {
+            System.out.println("Button 2 clicked");
+        }
     }
 }
--- MyLabel.java
+++ MyLabel.java
@@ -3,9 +3,8 @@ import javax.swing.*;
 
 public class MyLabel extends JLabel {
     MyLabel() {
-
         this.setText("Enter Fahrenheit: ");
-        this.setBounds(50, 200, 200, 50); // Adjust the position and size as needed
+        this.setBounds(50, 200, 200, 50);
         this.setFont(new Font("MV Boli", Font.PLAIN, 20));
     }
 }
